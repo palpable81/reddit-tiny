a5c32f19f866890eabee9632ef994ef8412affb6
Vinnie Prabhu <vinnie.prabhu@gmail.com>
2022-11-28 09:45:25 +0700

    Added Posts feature

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Posts from '../components/posts/Posts'
 import Header from '../components/header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Posts />
     </div>
   );
 }
---
+++ src/components/posts/Posts.jsx
@@ -0,0 +1,17 @@
+import './posts.css';
+
+function Posts() {
+  // const dispatch = useDispatch();
+
+  // const handleTermChange = ({target}) => {
+  //   dispatch(setSearchTerm(target.value));
+  // }
+
+  return (
+      <div className='posts'>
+        Temp text
+      </div>
+  );
+}
+
+export default Posts;
